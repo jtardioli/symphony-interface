aa7d5fdb07a357671b022578f85a9709642a2c93
jtardioli <joshua.tardioli@gmail.com>
2022-09-26 19:39:58 -0400

    clean up

--- src/pages/releases/new.tsx
+++ src/pages/releases/new.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-
 import Layout from "../../components/layouts/Layout";
-import { AiFillEye, AiFillDelete } from "react-icons/ai";
 import { LabeledInput, TrackInput } from "../../components/inputs";
 
 const NewRelease: NextPage = () => {
